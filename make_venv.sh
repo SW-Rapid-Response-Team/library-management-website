b1148cb7f90b131d1931e08809dfe1042dfe681e
KssssssH <rlatkdgus2627@gmail.com>
2023-01-31 10:36:18 +0000

    [ADD] rlatkdgus's commit install bootstrap

--- make_venv.sh
+++ make_venv.sh
@@ -8,5 +8,9 @@ python3 -m venv website_virtual_env
 source website_virtual_env/bin/activate
 
 sudo apt-get -y install python3-pip
+
+sudo apt-get -y install npm
+npm i bootstrap@5.3.0-alpha1
+
 pip install django==4.0.3
 pip install django-isbn-field==0.5.3
